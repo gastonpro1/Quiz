6eaf61efa6737f713ad7ca28ccd78ff928786e4e
Gaston <gastonmedjopro@gmail.com>
2023-01-26 17:12:11 +0100

    Ajout du footer dans le start 26/01 17:11 Gaston(Mickaël)

--- start.php
+++ start.php
@@ -1,38 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Start Page</title>
-    
+
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    
+
 </head>
+
 <body>
 
-<div class="menu">
-      
-      <h1> <strong> Panic QUIZ </strong> </h1>
-   
-</div>
+    <div class="menu">
 
-<section id="thème">
+        <h1> <strong> Panic QUIZ </strong> </h1>
 
-<h1 style="margin-top:15px";> <strong> Quiz sur le thème choisie : </strong>  <br><br>
+    </div>
 
-</h1>
+    <section id="thème">
 
-<a href="question.php?theme=<?= $_GET['theme']?>&&id=<?= $_GET['id']?>">
-<button style="background-color:rgb(48, 173, 163);" type="button" class="btn btn-primary btn-lg "> Démarrer le Quiz </button> </a>
+        <h1 style="margin-top:15px" ;> <strong> Quiz sur le thème choisie : </strong> <br><br>
 
-</section>
+        </h1>
+
+        <a href="question.php?theme=<?= $_GET['theme'] ?>&&id=<?= $_GET['id'] ?>">
+            <button style="background-color:rgb(48, 173, 163);" type="button" class="btn btn-primary btn-lg "> Démarrer le Quiz </button> </a>
+
+    </section>
 
 
-<script src="main.js"> </script>
+    <script src="main.js"> </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
-
+<?php include 'partials/footer.php' ?>
 
 </html>
